c8f3e996423ee167fcf594c0dfcc4952c049059e
Sierra Laurent <113314536+sierralaurent@users.noreply.github.com>
2023-11-15 18:31:07 +0000

    Update 5 files

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import "../styles/playlist.css";
 import data from "../data/data.json";
+import Nav from "./HomeMain/Nav";
 
 export default function Playlist() {
   const { playlistId } = useParams();
@@ -14,17 +15,26 @@ export default function Playlist() {
     <div className="playlist_container">
       <div className="playlist_item">
         <h1>{playlist.playlist}</h1>
-        <img src={playlist.image} alt={`Playlist ${playlist.playlist}`} />
-        <p>Creator: {playlist.creator}</p>
-
-        <h3>Songs:</h3>
-        <ul>
+        <img
+          src={playlist.image}
+          alt={`Playlist ${playlist.playlist}`}
+          className="playlist_image"
+        />
+        <div className="playlist_info">
+          <h2>Creator: {playlist.creator}</h2>
+          <h3>Songs:</h3>
+        </div>
+        <div className="playlist_songs">
           {playlist.song.map((song, j) => (
-            <li key={j}>
-              {song.name} - {song.artist} - {song.album}
-            </li>
+            <p key={j} className="songTitle">
+              {song.name}
+              <br />
+              <p className="songArtist">
+                {song.artist} - {song.album}
+              </p>
+            </p>
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
