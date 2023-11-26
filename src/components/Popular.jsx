cbde95b5622cd69c7da5bad717fdf87138124dcc
Sebin-Leeee <sebin.lee.work@gmail.com>
2023-11-26 12:25:57 -0800

    edit popular page layout

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,14 +1,25 @@
 import "../styles/myList.css";
-import MyList from "./HomeMain/MyList"
-import { Link } from "react-router-dom";
-
+import "../styles/home.css";
+import "../styles/popular.css";
+import MyList from "./HomeMain/MyList";
+import Nav from "./HomeMain/Nav";
+import Search from "./Search/Search";
+import Lyric from "./HomeMain/Lyric";
+import User from "./User";
 
 export default function Popular() {
-  
   return (
-    <div className="main_lists_itemContainer">
-        <div></div>
-      <MyList/>
+    <div className="home">
+      <div>
+        <Nav />
+      </div>
+      <div className="popular_list">
+        <MyList/>
+        <Lyric />
+      </div>
+      <div>
+        <User />
+      </div>
     </div>
   );
 }
